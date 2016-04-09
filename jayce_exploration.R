3862930603c2daea2e982c86b3dedf06a9d6a797
Jayce Rudig-Leathers <me@jayceleathers.com>
2016-04-09 11:26:24 -0400

    shrug changes

--- jayce_exploration.R
+++ jayce_exploration.R
@@ -1,3 +1,12 @@
 setwd('/Users/jayce/Desktop/bennie_data_fest/')
-purchase_path <- 'data/approved_data_purchase-v5.csv'
-purchases_all <- read.csv(purchase_path)
+###CONSTANTS###
+#data file paths
+purchases.path <- 'data/approved_data_purchase-v5.csv'
+adwords.path <- 'data/approved_adwords_v3.csv'
+ga.path <- 'data/approved_ga_data_v2.csv'
+#Others
+sample.size <- 10000
+#load purchases
+purchases.all <- read.csv(purchase_path)
+set.seed(666)
+sample.purchases <- purchases_all[sample(nrow(purchases_all), sample.size),]
