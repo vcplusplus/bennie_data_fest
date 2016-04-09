8d1c0327713a965a1f03eac609c24ada3c9fb7fc
Jayce Rudig-Leathers <me@jayceleathers.com>
2016-04-09 13:06:03 -0400

    adds variable for the difference between purchase date and event date

--- jayce_exploration.R
+++ jayce_exploration.R
@@ -7,6 +7,8 @@ ga.path <- 'data/approved_ga_data_v2.csv'
 #Others
 sample.size <- 10000
 #load purchases
-purchases.all <- read.csv(purchases.path, na.strings = 'NULL')
+purchases.all <- read.csv(purchases.path, na.strings = c("NULL","NA",""))
 set.seed(666)
-sample.purchases <- purchases.all[sample(nrow(purchases.all), sample.size),]
+purchases.sample <- purchases.all[sample(nrow(purchases.all), sample.size),]
+purchases.sample.subset <- subset(purchases.sample, select = c(1,10,13,14,15,17:22,27:29,32:35,38,45))
+purchases.sample.subset$event_date <- as.Date(purchases.sample.subset$event_dt)
