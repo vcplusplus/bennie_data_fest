e131000e8573bff837d767ea9d9ad5eb92b284fa
Jayce Rudig-Leathers <me@jayceleathers.com>
2016-04-09 12:08:46 -0400

    script refactoring

--- jayce_exploration.R
+++ jayce_exploration.R
@@ -7,6 +7,6 @@ ga.path <- 'data/approved_ga_data_v2.csv'
 #Others
 sample.size <- 10000
 #load purchases
-purchases.all <- read.csv(purchase_path)
+purchases.all <- read.csv(purchases.path, na.strings = 'NULL')
 set.seed(666)
-sample.purchases <- purchases_all[sample(nrow(purchases_all), sample.size),]
+sample.purchases <- purchases.all[sample(nrow(purchases.all), sample.size),]
