6c844cfd51e8fab8c551d36c3c7fb8b87c3f606d
Jayce Rudig-Leathers <me@jayceleathers.com>
2016-04-09 18:34:47 -0400

    refactoring

--- jayce_exploration.R
+++ jayce_exploration.R
@@ -10,5 +10,15 @@ sample.size <- 10000
 purchases.all <- read.csv(purchases.path, na.strings = c("NULL","NA",""))
 set.seed(666)
 purchases.sample <- purchases.all[sample(nrow(purchases.all), sample.size),]
-purchases.sample.subset <- subset(purchases.sample, select = c(1,10,13,14,15,17:22,27:29,32:35,38,45))
+#subset variables
+purchases.variables <- c(1,10,13,14,15,17:22,27:29,32:35,38,45)
+purchases.sample.subset <- subset(purchases.sample, select = purchases.variables)
+
+#compute number of days difference between transaction date and event date
 purchases.sample.subset$event_date <- as.Date(purchases.sample.subset$event_dt)
+purchases.sample.subset$purchase_date <- as.Date(purchases.sample.subset$sales_ord_tran_dt)
+purchases.sample.subset$event_purchase_difference <- purchases.sample.subset$event_date - purchases.sample.subset$purchase_date
+
+#trim junk events
+purchases.sample.subset.good <- purchases.sample.subset[purchases.sample.subset$la_valid_tkt_event_flg == 2,]
+str(purchases.sample.subset)
